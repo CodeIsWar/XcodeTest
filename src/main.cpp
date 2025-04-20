ccf45f35285befac0c6ec41c0220010c2dec87c9
CodeIsWar <105530634+CodeIsWar@users.noreply.github.com>
2025-04-20 17:25:20 +0800

    add interaction

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 int main() {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+    std::cout << "what is your name: ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!\n";
 }
--- xmake.lua
+++ xmake.lua
@@ -2,4 +2,6 @@ add_rules("mode.release")
 target("demo")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_cxxflags("/MD")
+    if is_plat("windows") then
+        add_cxxflags("/MD")
+    end
